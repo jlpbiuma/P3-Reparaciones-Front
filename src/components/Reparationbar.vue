931e435e08c8c97349655ab63dc60e9ac0ad6929
jlpbiuma <jpordomingo@iuma.ulpgc.es>
2022-11-23 17:36:25 +0000

    COMPONENTS: Reparation bar selected button work

--- src/components/Reparationbar.vue
+++ src/components/Reparationbar.vue
@@ -1,13 +1,29 @@
 <script setup>
-import {RouterLink} from 'vue-router'
+import { RouterLink } from 'vue-router'
+import { useAuthStore } from '../stores/authStore'
 </script>
 
 <template>
     <nav id="reparationBar">
         <RouterLink to="/newRepair"><button>CREATE NEW Repair!</button></RouterLink>
-        <RouterLink to="/unasignedRepairs"><button type="button" class="btn btn-outline-primary">Repairs</button></RouterLink>
-        <RouterLink to="/asignedRepairs"><button type="button" class="btn btn-outline-primary">Pending</button></RouterLink>
-        <RouterLink to="/doneRepairs"><button type="button" class="btn btn-outline-primary">History</button></RouterLink>
+        <RouterLink to="/unasignedRepairs" v-if="authStore.repairViewState == 'unasignedRepairs'" class="selected">
+            <button type="button" class="btn btn-outline-primary">Repairs</button>
+        </RouterLink>
+        <RouterLink to="/unasignedRepairs" v-else>
+            <button type="button" class="btn btn-outline-primary">Repairs</button>
+        </RouterLink>
+        <RouterLink to="/asignedRepairs" v-if="authStore.repairViewState == 'asignedRepairs'" class="selected">
+            <button type="button" class="btn btn-outline-primary">Pending</button>
+        </RouterLink>
+        <RouterLink to="/asignedRepairs" v-else>
+            <button type="button" class="btn btn-outline-primary">Pending</button>
+        </RouterLink>
+        <RouterLink to="/doneRepairs" v-if="authStore.repairViewState == 'doneRepairs'" class="selected">
+            <button type="button" class="btn btn-outline-primary">History</button>
+        </RouterLink>
+        <RouterLink to="/doneRepairs" v-else>
+            <button type="button" class="btn btn-outline-primary">History</button>
+        </RouterLink>
     </nav>
 </template>
 
@@ -15,6 +31,7 @@ import {RouterLink} from 'vue-router'
 export default {
     data() {
         return {
+            authStore: useAuthStore()
         }
     }
 }
@@ -24,5 +41,8 @@ export default {
 #reparationBar {
     display: flex;
     justify-content: space-around;
+}
+.selected {
+    background-color: #0d6efd;
 }
 </style> 
