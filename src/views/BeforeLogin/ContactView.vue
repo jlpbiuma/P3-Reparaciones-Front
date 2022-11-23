45ea8fd6987322504014ea31f2f71b368c75e764
VanesaRT <vanesarop5@gmail.com>
2022-11-23 13:46:47 +0000

    VIEWS: ContentView modifieD; HomeView: modified

--- src/views/BeforeLogin/ContactView.vue
+++ src/views/BeforeLogin/ContactView.vue
@@ -2,8 +2,74 @@
 </script>
 
 <template>
-    <h3>CONTACT US</h3>
-    hahahha
+    <div class="centered_text" style="max-width:80vw;">
+    <h2>CONTACT US</h2>
+    
+    <div class="row" >
+        <div class="row w-100">
+  <div class="col-lg-6 my-4">
+    <iframe 
+      src="https://www.google.com/maps/place/Bridge+to+Nowhere/@34.2833567,-117.7471725,20z/data=!4m5!3m4!1s0x80c322cfd9f1704f:0xd04401dc1101aa9c!8m2!3d34.283249!4d-117.7470737"
+      class="w-100" height="400" allowfullscreen="" loading="lazy"></iframe>
+  </div>
+  <div class="col-lg-6 my-4 d-flex align-items-end">
+    <div>
+      <h6>Find Us At</h6>
+      <p>Nowhere St, LP 35400</p>
+      <p>We will be pleased to assit you at the following telephone number: <strong>+34 123 45 67 89</strong>.</p>
+    </div>
+  </div>
+</div>
+</div>
+
+<br>
+<div class="container w-100" >
+<form id="contactForm">
+    <h5>Tell us what you need</h5>
+  <div class="mb-3">
+    <label class="form-label" for="name">Name</label>
+    <input class="form-control" id="name" type="text" placeholder="Name" />
+  </div>
+  <div class="mb-3">
+    <label class="form-label" for="emailAddress">Email Address</label>
+    <input class="form-control" id="emailAddress" type="email" placeholder="Email Address" />
+  </div>
+  <div class="mb-3">
+    <label class="form-label" for="message">Message</label>
+    <textarea class="form-control" id="message" type="text" placeholder="Message" style="height: 10rem;"></textarea>
+  </div>
+  <div class="d-grid">
+    <button id="submitbtn" class="btn btn-primary btn-lg" type="submit">Submit</button>
+  </div>
+</form>
+
+</div>
+<div class="card w-100" >
+    
+    <p class="card-text"><h4>&nbsp;Contact the Team on Github</h4></p>
+    <div class="card-group" >
+
+        <div class="card" id="team-profile" style="display:flex">
+        <a href="https://github.com/jlpbiuma">
+            <img src="../../../images/Jose Luis.jpg"  style=" width:10vw; border-radius: 10px;">
+            <p class="name"><strong> José Luis</strong></p>
+        </a></div>
+        <div class="card" id="team-profile" style="display:flex">
+        <a href="https://github.com/itsJosephV">
+            <img src="../../../images/Joseph.jpg"  style=" width:10vw; border-radius: 10px;">
+            <p class="name"><strong> Joseph</strong></p>
+        </a></div>
+
+        <div class="card" id="team-profile" style="display:flex"> 
+        <a href="https://github.com/VanesaMRP">
+            <img src="../../../images/Vanesa.jpg"  style=" width:10vw; border-radius: 10px;">
+            <p class="name"><strong> Vanesa</strong></p>
+        </a></div>
+        
+    </div>
+</div>
+</div>
+
 </template>
 
 <script>
@@ -18,14 +84,35 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.form {
-    background-color: rgb(181, 229, 255);
-    padding: 15px;
-
-    label {
-        display: block;
-        padding: 5px;
-        margin-top: 10px;
+.centered_text {
+    margin-left:auto;
+    margin-right:auto;
     }
+
+a{
+    color:black;
+    text-decoration: none; 
+}
+a:hover{
+    color:rgb(255, 145, 0);
+}
+.name{
+ font-size: larger;
+
+}
+#team-profile{
+    border-color: rgb(255, 255, 255);
+    justify-content: center;
+    align-items: center;
+}
+
+#submitbtn{
+    background-color:rgb(41, 41, 41);
+    border: 1px  rgb(82, 82, 82);
+    margin-bottom: 50px;
+}
+#submitbtn:hover{
+    background-color: rgb(82, 82, 82);
+    color:rgb(255, 145, 0);
 }
 </style>
--- src/views/BeforeLogin/HomeView.vue
+++ src/views/BeforeLogin/HomeView.vue
@@ -4,9 +4,9 @@ import {RouterLink} from 'vue-router'
 
 <template>
     <div class="row">
-  <div class="w-100">
+  <div class="w-100" >
     <div class="card">
-      <div class="card-body d-flex flex-column">
+      <div class="card-body d-flex flex-column" >
         <h1 class="card-title ">WELCOME TO JVJ-REPAIRS!</h1>
         <p class="card-text"><h3 id="slogan">We solve problems</h3></p>
         <div class="row-sm img-right">
@@ -16,36 +16,51 @@ import {RouterLink} from 'vue-router'
       </div>
     </div>
   </div>
-  <div class="w-50">
-    <div class="card">
-      <div class="card-body d-flex flex-column">
+  <div class="col-sm-6" style="max-height:80vw">
+    <div class="card w-100" style="max-height:82vw" id="news">
+      <div class="card-body d-flex flex-column" >
         <p class="card-text"><h3>News</h3></p>
         <p class="card-text"><h5>Apple and Google face gaming and mobile browser probe</h5></p>
         <img  class="img" style="width: auto; "  src="../../../images/gaming.jpg">
         <br>
-        <p class="card-text">Apple and Google's dominance over cloud gaming and mobile browsers will be investigated by the UK's competition regulator, it has announced.
+     
+        <p>
+        Apple and Google's dominance over cloud gaming and mobile browsers will be investigated by the UK's competition regulator, it has announced.
 
-Last year, a "market study" by the Competition and Markets Authority (CMA) concluded they had a "stranglehold" over operating systems, app stores and web browsers on mobile devices.
+        Last year, a "market study" by the Competition and Markets Authority (CMA) concluded they had a "stranglehold" over operating systems, app stores and web browsers on mobile devices.
 
-If the 18-month investigation finds an adverse effect on competition, the CMA can impose changes.
+        If the 18-month investigation finds an adverse effect on competition, the CMA can impose changes.
 
-Both companies deny the accusations.
-
-CMA interim chief executive Sarah Cardell said it wanted to ensure UK:
-<ul>
-<li><em>Consumers received the best new mobile data services</em></li>
-<li><em>Developers could invest in innovative new apps</em></li></ul>
-</p>
-
-        <div class="row-sm img-right">
-        <img  class="img" style="width: fit-content; float: right" >
-        </div>
+        Both companies deny the accusations.
+        <br>
+        CMA interim chief executive Sarah Cardell said it wanted to ensure UK:
+        <br>
+          <ul><em>Consumers received the best new mobile data services.</em></ul>
+          <ul><em>Developers could invest in innovative new apps.</em></ul>
+      </p> 
+  
+  
       </div>
     </div>
   </div>
-  <div class="w-50">
-    <div class="card">
+  
+  <div class="col-sm-6" style="max-height:80vw">
+    <div class="card w-100" id="content" style="max-height:40vw" >
       <div class="card-body d-flex flex-column">
+        <p class="card-text"><h4>SOME CONTENT</h4></p>
+        <div class="row-sm ">
+          <p >"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
+            eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
+            eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+        <img  class="img" style="width: 20vw; float: right"  src="">
+        </div>
+      </div>
+  </div>
+    <div class="card w-100" >
+      <div class="card-body d-flex flex-column" style="max-height:40vw">
         <p class="card-text"><h3>Download our APP</h3></p>
         <div class="row-sm img-right">
         <img  class="img" style="width: 20vw; float: right"  src="../../../images/app.png">
@@ -73,6 +88,21 @@ export default {
 </script>
 
 <style scoped >
+#news{
+  overflow: scroll;
+  text-align: justify;
+}
+#news::-webkit-scrollbar{
+  padding: 1rem;
+  max-height: 220px;
+  overflow-y: auto;
+}
+#content{
+  background-color: rgb(245, 245, 245);
+  border-color:  rgb(245, 245, 245);
+  overflow: scroll;
+  text-align: justify;
+}
 .card{
     border-color:  rgb(255, 255, 255);
     margin-bottom: 30px;
@@ -82,7 +112,7 @@ export default {
  border: 1px  rgb(29, 29, 29);
 }
 .btn:hover {
-      background-color: rgb(82, 82, 82) !important;
+      background-color: rgb(82, 82, 82);
       border: 1px  rgb(82, 82, 82);
     }
  #appbutton{
