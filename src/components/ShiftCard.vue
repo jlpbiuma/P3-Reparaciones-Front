98c95d96bf72769843cbe13d1a527e6c99fb35f4
jlpbiuma <jpordomingo@iuma.ulpgc.es>
2022-11-25 11:23:53 +0000

    Fixed card

--- src/components/ShiftCard.vue
+++ src/components/ShiftCard.vue
@@ -34,4 +34,17 @@ export default {
 </script>
 
 <style>
+#cardContainer {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    background-color: #F5F5F5;
+    border-radius: 10px;
+    box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.75);
+    margin: 10px;
+    padding: 10px;
+}
 </style>
